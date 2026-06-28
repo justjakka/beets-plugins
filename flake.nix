fe79ba5fdfdd2e79ba0d6fa2ceec74878d608e5c
jakka <jakka@jakka.foo>
2026-06-28 05:01:02 +0300

    uv2nix init

--- flake.nix
+++ flake.nix
@@ -3,38 +3,104 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
+    inputs@{
+      flake-parts,
+      uv2nix,
+      pyproject-nix,
+      pyproject-build-systems,
+      ...
+    }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.treefmt-nix.flakeModule
+      imports = [ inputs.treefmt-nix.flakeModule ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
       ];
-        systems = [
-          "x86_64-linux"
-          "aarch64-linux"
-        ];
       perSystem =
         {
+          system,
           pkgs,
+          lib,
           ...
         }:
+        let
+          workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+          overlay = workspace.mkPyprojectOverlay {
+            sourcePreference = "wheel";
+          };
+          editableOverlay = workspace.mkEditablePyprojectOverlay {
+            root = "$REPO_ROOT";
+          };
+          python = lib.head (
+            pyproject-nix.lib.util.filterPythonInterpreters {
+              inherit (workspace) requires-python;
+              inherit (pkgs) pythonInterpreters;
+            }
+          );
+          pythonSets =
+            (pkgs.callPackage pyproject-nix.build.packages {
+              inherit python;
+            }).overrideScope
+              (
+                lib.composeManyExtensions [
+                  pyproject-build-systems.overlays.wheel
+                  overlay
+                ]
+              );
+          pythonSet = pythonSets.${system}.overrideScope editableOverlay;
+          virtualenv = pythonSet.mkVirtualEnv "beets-plugins" workspace.deps.all;
+        in
         {
           devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              ruff
-              nixfmt
-              nixd
-              python3
-              pyright ];
+            packages =
+              with pkgs;
+              [
+                nixd
+                nixfmt
+                pyright
+                ruff
+                uv
+              ]
+              ++ virtualenv;
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = pythonSet.python.interpreter;
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+            shellHook = ''
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+            '';
           };
-
           treefmt = {
             projectRootFile = "flake.nix";
             programs = {
-              nixfmt.enable = true;
-              ruff.enable = true;
+              nixfmt = {
+                enable = true;
+                strict = true;
+              };
+              ruff = {
+                check = true;
+                format = true;
+              };
             };
           };
         };
